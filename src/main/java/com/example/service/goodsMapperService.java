522546337168d28c8cd9b06be5140eeb1a19f205
MingYueyuni <MingYueyuni@Gmail.com>
2020-06-15 21:52:01 +0800

    test

--- src/main/java/com/example/service/goodsMapperService.java
+++
@@ -1,7 +0,0 @@
-package com.example.service;
-
-import com.example.pojo.goods;
-
-public interface goodsMapperService {
-    public goods findgoodsById(int id);
-}
--- src/main/java/com/example/service/membersMapperService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.pojo.members;
-
-import java.util.List;
-
-public interface membersMapperService {
-    public List<members> findAllmembers();
-}
